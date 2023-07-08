12f06b2118e959e5ed94bbb2213f231af0b56e44
Hicruben <hiruben@gmail.com>
2023-07-08 22:21:04 +0800

    release for WAIC2003_shanghai

--- server.js
+++ server.js
@@ -247,7 +247,7 @@ board.on("ready", function () {
           video_to_play["middle"] = 6;
         } else if (msg == 2) {
           video_to_play["left"] = 4;
-          video_to_play["right"] = 7;
+          video_to_play["middle"] = 7;
         } else if (msg == 3) {
           video_to_play["left"] = 5;
           video_to_play["middle"] = 8;
